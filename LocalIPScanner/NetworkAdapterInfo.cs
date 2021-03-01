cfc8a943a71f087c17d5c47374c1d3df65800bca
Keith <49873540+MangoDevx@users.noreply.github.com>
2021-03-01 03:28:14 -0500

    First version

---
+++ LocalIPScanner/NetworkAdapterInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+
+namespace LocalIPScanner
+{
+    public class NetworkAdapterInfo
+    {
+        public IPAddress Ip { get; set; }
+        public int Index { get; set; }
+        public int InterfaceIndex { get; set; }
+        public string Caption { get; set; }
+        public string Description { get; set; }
+        public bool InUse { get; set; }
+        public DhcpInfo DhcpInfo { get; set; }
+        public DnsInfo DnsInfo { get; set; }
+    }
+
+    public class DhcpInfo
+    {
+        public DhcpInfo(bool enabled, string server, DateTime expires, DateTime obtained)
+        {
+            DhcpEnabled = enabled;
+            DhcpServer = server;
+            DhcpLeaseExpires = expires;
+            DhcpLeaseObtained = obtained;
+        }
+        public bool DhcpEnabled { get; private set; }
+        public string DhcpServer { get; private set; }
+        public DateTime DhcpLeaseExpires { get; private set; }
+        public DateTime DhcpLeaseObtained { get; private set; }
+    }
+
+    public class DnsInfo
+    {
+        public DnsInfo(string domain, string host, IPAddress[] searchOrder)
+        {
+            DnsDomain = domain;
+            DnsHostName = host;
+            DnsServerSearchOrder = searchOrder;
+        }
+        public string DnsDomain { get; private set; }
+        public string DnsHostName { get; private set; }
+        public IPAddress[] DnsServerSearchOrder { get; private set; }
+    }
+}
--- LocalIPScanner/Program.cs
+++ LocalIPScanner/Program.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Management;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LocalIPScanner
@@ -15,6 +19,8 @@ namespace LocalIPScanner
         {
             await new Program().BeginScanning();
         }
+
+        private Stopwatch _sw = new Stopwatch();
 
         public async Task BeginScanning()
         {
@@ -27,7 +33,7 @@ namespace LocalIPScanner
                 var nic = allNics[i];
                 nics.Add(i, nic);
             }
-            Console.WriteLine("Please select the adapter you wish to modify/revert by typing the corresponding number.");
+            Console.WriteLine("Please select the adapter you wish to use as the base of the scan by typing the corresponding number.");
             foreach (var possibleNic in nics)
             {
                 Console.WriteLine($"{possibleNic.Key}: {possibleNic.Value.Name}");
@@ -78,30 +84,59 @@ namespace LocalIPScanner
             var baseIp = $"{baseIpSplit[0]}.{baseIpSplit[1]}.{baseIpSplit[2]}.";
             Console.WriteLine($"Base address: {baseIp}");
             var possibleAddresses = new List<IPAddress>();
-            for (var i = 0; i <= 255; i++)
+            for (var i = 1; i <= 255; i++)
             {
                 if (!IPAddress.TryParse($"{baseIp}{i}", out var builtIp))
                 {
                     Console.WriteLine($"Failed to parse address {baseIp}{i}");
                 }
+                possibleAddresses.Add(builtIp);
             }
-            await GetNicInformation(possibleAddresses);
+            Console.WriteLine("Possible DHCP addresses built\n");
+            var jsonOutput = await GetNicInformation(possibleAddresses);
+            OutputInformation(jsonOutput);
+
         }
 
-        private async Task GetNicInformation(List<IPAddress> possibleAddresses)
+        private async Task<string> GetNicInformation(IEnumerable<IPAddress> possibleAddresses)
         {
-            foreach (var ip in possibleAddresses)
+            Console.WriteLine("Beginning to compile all network information");
+            _sw.Start();
+            var compiledNetworkInfo = await Task.WhenAll(possibleAddresses.Select(GetBasicAdapterInfo).ToArray());
+            _sw.Stop();
+            Console.WriteLine($"All network information compiled. Count: {compiledNetworkInfo.Length} Elapsed Time: {_sw.ElapsedMilliseconds}ms");
+            _sw.Reset();
+            return JsonConvert.SerializeObject(compiledNetworkInfo);
+        }
+
+        private void OutputInformation(string jsonOutput)
+        {
+            Console.WriteLine("Output to text? y/n");
+            var input = Console.ReadLine().ToLowerInvariant();
+            while (true)
             {
-                var basicAdapterInfo = GetBasicAdapterInfo(ip);
+                if (input.Equals("y"))
+                {
+                    File.WriteAllText($@"NIOutput@{DateTime.Now}", jsonOutput);
+                    break;
+                }
+
+                if (input.Equals("n"))
+                {
+                    Console.WriteLine(jsonOutput);
+                    break;
+                }
+
+                Console.WriteLine("Invalid input. Please enter y or n");
+                Thread.Sleep(1);
             }
         }
 
-        private async Task<NetworkAdapter> GetBasicAdapterInfo(IPAddress ip)
+        private async Task<NetworkAdapterInfo> GetBasicAdapterInfo(IPAddress ip)
         {
-            var adapter = new NetworkAdapter { Ip = ip };
-
+            var adapter = new NetworkAdapterInfo { Ip = ip };
             var ping = new Ping();
-            var response = await ping.SendPingAsync(ip, 2000);
+            var response = await ping.SendPingAsync(ip, 400);
             adapter.InUse = response.Status == IPStatus.Success;
             ping.Dispose();
 
@@ -110,8 +145,8 @@ namespace LocalIPScanner
 
             foreach (ManagementObject objMo in searcherGet)
             {
-                var ipEnabled = bool.Parse(objMo["IPEnabled"].ToString() ?? throw new InvalidOperationException());
-                if (!ipEnabled) continue;
+                if (!bool.Parse(objMo["IPEnabled"].ToString()))
+                    continue;
                 if (!objMo["IPAddress"].ToString().Contains(ip.ToString())) continue;
 
                 var objIndex = objMo["Index"].ToString();
@@ -132,9 +167,52 @@ namespace LocalIPScanner
 
                 adapter.Caption = objMo["Caption"].ToString();
                 adapter.Description = objMo["Description"].ToString();
+                return adapter;
             }
-
+            SetDhcpInfo(adapter, ip);
+            SetDnsInfo(adapter, ip);
             return adapter;
+        }
+
+        private void SetDhcpInfo(NetworkAdapterInfo adapter, IPAddress ip)
+        {
+            var searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_NetworkAdapterConfiguration");
+            var searcherGet = searcher.Get();
+
+            foreach (ManagementObject objMo in searcherGet)
+            {
+                if (!bool.Parse(objMo["IPEnabled"].ToString()))
+                    continue;
+                if (!objMo["IPAddress"].ToString().Contains(ip.ToString())) continue;
+
+                var enabled = bool.Parse(objMo["DHCPEnabled"].ToString());
+                var server = objMo["DHCPServer"].ToString();
+                var leaseExpires = ManagementDateTimeConverter.ToDateTime(objMo["DHCPLeaseExpires"].ToString());
+                var leaseObtained = ManagementDateTimeConverter.ToDateTime(objMo["DHCPLeaseObtained"].ToString());
+                adapter.DhcpInfo = new DhcpInfo(enabled, server, leaseExpires, leaseObtained);
+                break;
+            }
+        }
+
+        private void SetDnsInfo(NetworkAdapterInfo adapter, IPAddress ip)
+        {
+            var searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_NetworkAdapterConfiguration");
+            var searcherGet = searcher.Get();
+
+            foreach (ManagementObject objMo in searcherGet)
+            {
+                if (!bool.Parse(objMo["IPEnabled"].ToString()))
+                    continue;
+                if (!objMo["IPAddress"].ToString().Contains(ip.ToString())) continue;
+
+                var domain = objMo["DNSDomain"].ToString();
+                var host = objMo["DNSHostName"].ToString();
+                var searchOrder = (string[])objMo["DNSServerSearchOrder"];
+                var ipOne = IPAddress.Parse(searchOrder[0]);
+                var ipTwo = IPAddress.Parse(searchOrder[1]);
+                adapter.DnsInfo = new DnsInfo(domain, host, new[] { ipOne, ipTwo });
+                break;
+            }
         }
     }
 }
